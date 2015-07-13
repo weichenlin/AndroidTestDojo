d7cb41d44123ee5bcd4c507b306136207a6cb1f2
Weicheng Lin <wclinhub@sp36.net>
2015-07-13 10:49:01 +0800

    first commit

---
+++ app/src/main/java/cc/aznc/utdojo_1/Average.java
@@ -0,0 +1,61 @@
+package cc.aznc.utdojo_1;
+
+import android.widget.TextView;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+
+/**
+
+ * Created by Chris on 2015/7/12.
+
+ */
+
+public class Average {
+
+    private ArrayList<Person> persons;
+
+    private TextView view;
+
+    private NumberFormat nf = NumberFormat.getInstance();
+
+
+
+    public Average(ArrayList data, TextView view) {
+
+        this.persons = data;
+
+        this.view = view;
+
+        nf.setMaximumFractionDigits(3);
+
+    }
+
+
+
+    public void notifyDataSetChanged() {
+
+        view.setText(nf.format(getAVG()));
+
+    }
+
+
+
+    private float getAVG() {
+
+        int counts = 0;
+
+        int total = 0;
+
+        for(Person person : persons) {
+
+            total += person.getPay();
+
+            counts++;
+
+        }
+
+        return total / counts;
+
+    }
+
+}
---
+++ app/src/main/java/cc/aznc/utdojo_1/Person.java
@@ -0,0 +1,14 @@
+package cc.aznc.utdojo_1;
+
+public class Person {
+    private int pay = 0;
+
+    public Person(int pay)
+    {
+        this.pay = pay;
+    }
+
+    public int getPay() {
+        return pay;
+    }
+}
